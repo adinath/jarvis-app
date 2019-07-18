762e3036d17278610d8370b377b090a992cdf627
Adinath Shirsath <adinaths@gmail.com>
2019-07-18 09:49:35 -0400

    passing data to child component

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core'
 })
 
 export class EventsListComponent {
-    events = {
+    events = [{
         id: 1,
         name: 'Angular Connect',
         date: '1/1/2020',
@@ -17,5 +17,34 @@ export class EventsListComponent {
             city: 'Jersey City',
             country: 'USA'
         }
+    }
+    ,{
+        id: 2,
+        name: 'Angular Disconnect',
+        date: '1/1/2020',
+        time: '10:00 am',
+        price: '299.00',
+        location: {
+            address: '33 Hudson st',
+            city: 'Jersey City',
+            country: 'USA'
+        }
+    }
+    ,{
+        id: 4,
+        name: 'Angular Returns',
+        date: '1/1/2020',
+        time: '10:00 am',
+        price: '299.00',
+        location: {
+            address: '33 Hudson st',
+            city: 'Jersey City',
+            country: 'USA'
+        }
+    }
+]
+
+    handleEventClicked(data){
+        console.log(data)
     }
 }
