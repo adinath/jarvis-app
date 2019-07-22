43bbe15ee249c3d51c4db6998b047d55bb3dbfcd
Adi <adinaths@gmail.com>
2019-07-22 15:46:28 -0400

    add event model

--- src/app/events/shared/events.service.ts
+++ src/app/events/shared/events.service.ts
@@ -1,51 +1,52 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Subject, Observable } from 'rxjs';
+import { IEvent } from './event-model'
 
 @Injectable()
 export class EventService {
-    getEvents() {
-        let subject = new Subject()
+    getEvents(): Observable<IEvent[]>{
+        let subject = new Subject<IEvent[]>()
         setTimeout(() => {
             subject.next(EVENTS)
             subject.complete()
         }, 100);
         return subject
     }
-    getEvent(id:any) {
+    getEvent(id: any) {
         return EVENTS[id]
     }
 }
 
-const EVENTS = [{
+const EVENTS : IEvent[]= [{
     id: 0,
     name: 'Angular Connect',
-    date: '1/1/2020',
+    date: new Date('1/1/2020'),
     time: '10:00 am',
-    price: '299.00',
+    price: 299.00,
     location: {
         address: '33 Hudson st',
         city: 'Jersey City',
         country: 'USA'
     }
 }
-,{
+    , {
     id: 1,
     name: 'Angular Disconnect',
-    date: '1/1/2020',
+    date: new Date('1/1/2020'),
     time: '10:00 am',
-    price: '299.00',
+    price: 299.00,
     location: {
         address: '33 Hudson st',
         city: 'Jersey City',
         country: 'USA'
     }
 }
-,{
+    , {
     id: 2,
     name: 'Angular Returns',
-    date: '1/1/2020',
+    date: new Date('1/1/2020'),
     time: '10:00 am',
-    price: '299.00',
+    price: 299.00,
     location: {
         address: '33 Hudson st',
         city: 'Jersey City',
